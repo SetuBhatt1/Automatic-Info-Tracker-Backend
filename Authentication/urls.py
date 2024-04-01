6b6752d62c3d04ca3177727a536d45480e10dda8
Setu Bhatt <bansetu1@gmail.com>
2024-04-01 18:55:35 +0530

    url changed

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('service/',include('webscraping.urls'))
+    path('service/', include('webscrape_data.urls')),
 ]
