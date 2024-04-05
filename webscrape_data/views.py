e008ac33b5650969307cbdbb27fa8499d731baf8
Setu Bhatt <bansetu1@gmail.com>
2024-04-05 09:14:41 +0530

    minor changes

--- webscrape_data/views.py
+++ webscrape_data/views.py
@@ -455,18 +455,18 @@ class RegisterBusinessView(APIView):
             elif business_type == 'Pg':
                 gender_type = registration_form_data.get('for_gender')
                 if gender_type == 'G':
-                    girls_pg_serializer = GirlsHostelSerializer(data=registration_form_data, partial=True)
+                    girls_pg_serializer = GirlsPgSerializer(data=registration_form_data, partial=True)
                     if girls_pg_serializer.is_valid():
                         girls_pg_serializer.save(vid=vendor)
                     else:
                         return Response(girls_pg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
                 elif gender_type == 'B':
-                    boys_pg_serializer = BoysHostelSerializer(data=registration_form_data, partial=True)
+                    boys_pg_serializer = BoysPgSerializer(data=registration_form_data, partial=True)
                     if boys_pg_serializer.is_valid():
                         boys_pg_serializer.save(vid=vendor)
                     else:
                         return Response(boys_pg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-                pass
+
             elif business_type == 'T':
                 tiffin_serializer = TiffinSerializer(data=registration_form_data)
                 if tiffin_serializer.is_valid():
