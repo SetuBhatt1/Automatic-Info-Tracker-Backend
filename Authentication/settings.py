1c8c203e6eb6cd4bd091d59add8572a103d166aa
Setu Bhatt <bansetu1@gmail.com>
2024-04-05 09:48:10 +0530

    deployment config

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-15c4f*p1_yzc8mr$#6axsqby-xax$gp=lx&ny&+6d0)ax8qjhp
 DEBUG = True
 
 # '.vercel.app'
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'vercel.app','*']
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
